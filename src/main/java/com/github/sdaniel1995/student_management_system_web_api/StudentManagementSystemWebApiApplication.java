082c365284bbdbad96d1d647c30cc420da050a00
Shane Daniel <danielshane861@gmail.com>
2022-09-06 03:37:08 -0400

    Initial commit

---
+++ src/main/java/com/github/sdaniel1995/student_management_system_web_api/StudentManagementSystemWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.sdaniel1995.student_management_system_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sdaniel1995/student_management_system_web_api/StudentManagementSystemWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sdaniel1995.student_management_system_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
